69fcadbc940a7edac3c5e5c1363fdafd09ffba6b
euc1 <jamesharrington585@hotmail.com>
2022-01-01 15:40:46 -0500

    added annotations

--- Develop/script.js
+++ Develop/script.js
@@ -3,7 +3,7 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Create Arrays of characters
+//Arrays of characters
 var lowerArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numberArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
@@ -12,7 +12,7 @@ var specialCharArray = ["!", "#", "$", "%", "&", "*", "+", "-", ".", "/", ":", "
 
 
 function generatePassword() {
-//create prompts for length, upper/lower case, numbers and special characters
+//Prompt for length
 var passwordLength = prompt("Choose a password length between 8 and 128 characters")
 if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
   alert("Please enter a number between 8 and 128")
@@ -20,6 +20,8 @@ if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
 }
 
 alert("Your password will have " + passwordLength + " characters");
+
+//Prompts for upper/lower case, numbers and special characters
 
 var chosenChars = [];
 
@@ -52,6 +54,7 @@ if (!lowerCase && !upperCase && !numeric && !specialChar) {
   return null;
 }
 
+// for loop and randomizer to ultimately get the password
 var finalPassword = " ";
 for (var chosenLength = 0; chosenLength < passwordLength; chosenLength++) {
   finalPassword = finalPassword + chosenChars[Math.floor(Math.random()* chosenChars.length)];
