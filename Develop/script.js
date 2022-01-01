bc73c2212aa05ffa595fb1e2d10a1fefc3feb720
euc1 <jamesharrington585@hotmail.com>
2022-01-01 15:09:05 -0500

    finished for loop and.. everything else. I can't believe it worked.

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,8 @@
 // Assignment code here
 
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
 // Create Arrays of characters
 var lowerArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
@@ -16,32 +19,32 @@ if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
   return null;
 }
 
-alert("Your password will have " + passwordLength + " characters")
+alert("Your password will have " + passwordLength + " characters");
 
-var chosenChars = []
+var chosenChars = [];
 
 var lowerCase = confirm("Do you want lower case letters?")
 if (lowerCase) {
   chosenChars = lowerArray.concat(chosenChars)
-  alert("Lower case you shall have!")
+  alert("Lower case you shall have!");
 }
 
 var upperCase = confirm("Do you want upper case letters?")
 if (upperCase) {
   chosenChars = upperArray.concat(chosenChars)
-  alert("Some caps. Nice choice.")
+  alert("Some caps. Nice choice.");
 }
 
 var numeric = confirm("Do you want numbers?")
 if (numeric) {
   chosenChars = numberArray.concat(chosenChars)
-  alert("Numbers for you")
+  alert("Numbers for you");
 }
 
 var specialChar = confirm("Do you want special characters?")
-if (specialCharArray) {
+if (specialChar) {
   chosenChars = specialCharArray.concat(chosenChars)
-  alert("There's gonna be some special characters in it now")
+  alert("you're gonna get special characters now");
 }
 
 if (!lowerCase && !upperCase && !numeric && !specialChar) {
@@ -49,16 +52,15 @@ if (!lowerCase && !upperCase && !numeric && !specialChar) {
   return null;
 }
 
-var finalPassword = []
-for (let index = 0; index < array.length; index++) {
-  const element = array[index];
-  
+var finalPassword = " ";
+for (var chosenLength = 0; chosenLength < passwordLength; chosenLength++) {
+  finalPassword = finalPassword + chosenChars[Math.floor(Math.random()* chosenChars.length)];
 }
+
+return finalPassword;
 
 }
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# Password Generator Starter Code
+# Password Generator
+
+## Purpose
+To randomly generate a password based on the user's choice of Uppercase, Lowercase, Numeric and Special characters. Upon making all of their specific choices, the random password will appear for them to copy.
+
+## Built with
+*HTML
+
+*CSS
+
+*Javascript
+
+## Website
+https://eucalyptus1.github.io/horiseon-challenge/
+
+## Contribution
+Made by James
